ecefd5b52c75155ebbdd90be1f04c04634796f20
tossout1 <arnavsrivastava237@gmail.com>
2025-05-26 09:53:12 -0400

    Add files via upload

---
+++ Deployment-Hackathon-app/route/AI.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load the CSV file
+df = pd.read_csv('Form_AI.csv')
+
+# Get the value in row 2 (index 1), column 'name'
+
+# Function that returns the same cell value
+def ai_give_response(input):
+    cell_value = df.at[f"{input}", 'output']
+    return cell_valuec
